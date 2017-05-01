05e769edef656f4c6356bbf2787794453b65ec0c
Blake <lakebayomackway@gmail.com>
2017-05-01 14:32:44 -0700

    add app-root, more html and minimal css.

---
+++ app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <div class="jumbotron">
+    <h1>India Recipe Box</h1>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <p>content</p>
+      </div>
+    </div>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
